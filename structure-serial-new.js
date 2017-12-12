6e8f81ec8a6cb0e8dab341cd7747f926ac085311
chillchillchou <33140257+chillchillchou@users.noreply.github.com>
2017-12-12 03:35:07 -0500

    Add files via upload

--- structure-serial-new.js
+++ structure-serial-new.js
@@ -16,6 +16,10 @@ let s;
 let l;
 let ipt;
 
+
+var c;
+var ctx;
+
 function preload() {
   cityData = loadJSON("cityobject.json");
 }
@@ -23,19 +27,23 @@ function preload() {
 // var sunlight; //slider that indicates sunlight
 
 function setup() {
-
+  ipt = createInput();
+  // ipt.position(20, 65);
+  // ipt.style(z-index,10);
   //noCanvas();
   //create a canvas
-  let c = createCanvas(windowWidth, windowHeight);
-  background(0, 50);
+  c = createCanvas(windowWidth, windowHeight);
+  ctx = c.drawingContext;
+
+  noStroke();
   c.parent("#main-container");
+
   //create something for the rotating drawing
   x1 = 0;
   y1 = 0;
   // c.position(width/2, height/2);
+  //
 
-  ipt = createInput();
-  ipt.position(20, 65);
 
 
 
@@ -44,11 +52,11 @@ function setup() {
 
 
   //create serial comunnication
-  serial = new p5.SerialPort(); // make a new instance of  serialport librar
-  serial.on('list', printList); // callback function for serialport list event
-  serial.on('data', serialEvent); // callback for new data coming in
-  serial.list(); // list the serial ports
-  serial.open("/dev/cu.usbmodem1411"); // open a port
+  // serial = new p5.SerialPort(); // make a new instance of  serialport librar
+  // serial.on('list', printList); // callback function for serialport list event
+  // serial.on('data', serialEvent); // callback for new data coming in
+  // serial.list(); // list the serial ports
+  // serial.open("/dev/cu.usbmodem1411"); // open a port
 
   //create a slider to control what time to display for each city. The value of the slider represents the timezone that gets the most sunlight or is 12:00 at noon.
   //e.g.: if slider.value=0, then cities in timezone 0 are at 12:00pm. All other timezone's time change accordingly.
@@ -59,13 +67,45 @@ function setup() {
 function draw() {
 
   //try to draw the rotating thing
-  // background(0, 50);
-  if (ipt.value()) {
+  // background(250,255,255,10);
+
     timeTest = ipt.value();
     console.log(timeTest);
-  }
-  drawIndicator(timeTest);
+    drawGradient(timeTest);
+  // push();
+  // translate(width / 2, height / 2);
+  // var gradX = mouseX - width / 2;
+  // var gradY = mouseY - height / 2;
+  // var gradient = ctx.createRadialGradient(0, 0, 400, gradX, gradY, 0);
+  // gradient.addColorStop(0, "black");
+  // // gradient.addColorStop(0.3,"grey");
+  // gradient.addColorStop(1, "orange");
+  // ctx.fillStyle = gradient;
+  // ellipse(0, 0, 800, 800);
+  // pop();
+  // drawIndicator(timeTest);
 
+
+}
+
+function drawGradient(i){
+
+  push();
+  translate(width / 2, height / 2);
+
+  //gradX=R*cos(a),gradY=R*sin(a)
+  //a=270+*15*i
+  var gradX=400*cos(radians(90+15*timeTest*(-1)));
+  var gradY=400*sin(radians(90+15*timeTest*(-1)));
+  // var gradX = mouseX - width / 2;
+  // var gradY = mouseY - height / 2;
+  var gradient = ctx.createRadialGradient(0, 0, 400, gradX, gradY, 0);
+  gradient.addColorStop(0, "black");
+  // gradient.addColorStop(0.3,"grey");
+  gradient.addColorStop(1, "orange");
+  ctx.fillStyle = gradient;
+  ellipse(0, 0, 800, 800);
+  pop();
 }
 
 function updateImages(t) {
@@ -103,7 +143,7 @@ function updateImages(t) {
     timezoneContainer.id("container" + String(i));
     timezoneContainer.class("container");
     timezoneContainer.position(positionX, positionY);
-    document.getElementById("container" + String(i)).style.transform = "rotate(" + String(i * 15) + "deg) translate(0px, 400px)";
+    document.getElementById("container" + String(i)).style.transform = "rotate(" + String(i * (-1) * (15)) + "deg) translate(0px, 400px)";
 
     timezoneContainer.parent("#image-container");
     //loop through each city
@@ -119,7 +159,6 @@ function updateImages(t) {
 
         // currentImage.position(positionX, positionY);
         // currentImage.style.transform = "rotate(45deg)";
-
 
         currentImage.parent(cityContainer);
         currentImage.show();
@@ -177,25 +216,26 @@ function serialEvent() {
     maxReading = Math.max.apply(Math, fromSerials);
     minReading = Math.min.apply(Math, fromSerials);
     // maxTimezone=fromSerials.indexOf(maxReading);
-    if ((maxReading - minReading) > 100) {
+    if ((maxReading - minReading) > 15) {
       if (fromSerials.indexOf(maxReading) != maxTimezone) {
         maxTimezone = fromSerials.indexOf(maxReading);
         console.log("max timezone is" + maxTimezone);
         updateImages(maxTimezone);
-        drawIndicator(maxTimezone);
-        document.getElementById("container" + String(infexOf(maxReading))).style.color = "yellow";
+        drawIndicator(fromSerials.indexOf(maxReading));
+        console.log(maxTimezone);
+        document.getElementById("container" + String(fromSerials.indexOf(maxReading))).style.color = "yellow";
         // console.log(fromSerials);
       }
     } else {
       sleep();
     }
-
   }
 }
 
 function sleep() {
   document.getElementById("label-container").innerHTML = "";
   document.getElementById("image-container").innerHTML = "";
+  // background(0,90);
   //time is the time to display for each timezone
   var timezones = cityData.timezones; //access the timezone array in json file
   var positionX = window.innerWidth / 2; //specify initial x position for the first column of photos (timezone 0)
@@ -209,7 +249,7 @@ function sleep() {
     timezoneContainer.id("container" + String(i));
     timezoneContainer.class("container");
     timezoneContainer.position(positionX, positionY);
-    document.getElementById("container" + String(i)).style.transform = "rotate(" + String(i * 15) + "deg) translate(0px, 400px)";
+    document.getElementById("container" + String(i)).style.transform = "rotate(" + String((-1) * i * 15) + "deg) translate(0px, 400px)";
     timezoneContainer.parent("#image-container"); //loop through each city
     for (j = 0; j < timezones[i].length; j++) {
       var currentCity = timezones[i][j];
@@ -243,10 +283,10 @@ function sleep() {
 
 
 function drawIndicator(i) {
-  background(0, 95);
+  // background(0, 95);
   push();
   translate(width / 2, height / 2);
-  rotate(radians(i * 15+180));
+  rotate(radians(180 + i * (-1) * 15));
   scale(2);
   colorMode(HSL);
   // fill(47,100,70,0.04);
@@ -266,13 +306,16 @@ function drawIndicator(i) {
 
   //fill(360,100,100,random(0.5,0.95));
   stroke(360, 100, frameCount % 360, 1);
-  line(0, -10, 0, -150);
+  strokeWeight(2);
+  line(0, 0, 0, -150);
   x1 = x1 + random(-2, 2);
   y1 = y1 + random(-2, 2);
   //x1=20
-  pop();
+
 
 }
+
+
 
 // window.onload = function() {
 //   Maptastic("main-container");
